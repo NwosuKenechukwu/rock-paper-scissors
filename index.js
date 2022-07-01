f2cb855b8ff1c4543f7341395fdddb2eedec415b
NwosuKenechukwu <kenechukwugn@gmail.com>
2022-07-01 12:18:39 +0100

    Rock, Paper, Scissors Game
    Created the project from scratch to practice my skills based on what I've learned. Implemented a scoring system, a reset game feature and very basic game logic

---
+++ index.js
@@ -0,0 +1,105 @@
+"use strict";
+
+let plScore = document.getElementById("playerScore");
+let comScore = document.getElementById("comScore");
+let updateText = document.getElementById("update");
+const start = document.querySelector(".startGame");
+const newGame = document.querySelector(".newGame");
+const picks = document.querySelectorAll(".option");
+
+const options = ["rock", "paper", "scissors"];
+const computerPlay = function () {
+  return options[Math.trunc(Math.random() * options.length)];
+};
+
+let playerPick;
+
+picks.forEach((pick) => {
+  pick.addEventListener("click", function () {
+    updateText.textContent = "Waiting...";
+    pick.style["pointer-events"] = "none";
+    setTimeout(() => {
+      let outcome = playRound(pick.getAttribute("id"), computerPlay());
+      console.log(outcome);
+      updateScore(outcome);
+
+      pick.style["pointer-events"] = "auto";
+    }, 1000);
+  });
+});
+
+const playRound = function (playerSelection, computerSelection) {
+  let winText, loseText, drawText;
+  if (playerSelection.toLowerCase() === computerSelection) {
+    drawText = `This round ended in a draw. You both chose ${playerSelection}.`;
+    updateText.textContent = drawText;
+    return drawText;
+  }
+  if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    loseText = `You lost this round! Computer chose ${computerSelection} and ${computerSelection.toLowerCase()} beats ${playerSelection}`;
+    updateText.textContent = loseText;
+    return loseText;
+  } else {
+    winText = `You won this round! Computer chose ${computerSelection} and ${playerSelection} beats ${computerSelection.toLowerCase()}`;
+    updateText.textContent = winText;
+    return winText;
+  }
+};
+
+const whoWon = function () {
+  let finalOutcome = "";
+
+  if (+plScore.textContent === 2) {
+    finalOutcome = `You Won! 🏆`;
+  }
+  if (+comScore.textContent === 2) {
+    finalOutcome = `You Lost! 😢 Rematch? 😤`;
+  }
+  setTimeout(() => {
+    document.querySelector(".options").classList.add("hide");
+    document.getElementById("select").style.display = "none";
+    updateText.style.fontSize = "3vw";
+    updateText.textContent = finalOutcome;
+  }, 1000);
+};
+
+const game = function () {
+  document.querySelector(".options").classList.remove("hide");
+  document.getElementById("select").style.display = "flex";
+  updateText.textContent = "";
+  plScore.textContent = 0;
+  comScore.textContent = 0;
+  newGame.classList.remove("hide");
+  updateText.style.fontSize = "2vw";
+};
+
+const updateScore = function (outcome) {
+  let gameOutcome = outcome;
+
+  if (gameOutcome.split(" ").includes("won")) {
+    plScore.textContent = +plScore.textContent + 1;
+  }
+  if (gameOutcome.split(" ").includes("lost")) {
+    comScore.textContent = +comScore.textContent + 1;
+  }
+  if (gameOutcome.split(" ").includes("draw")) {
+    console.log("draw");
+  }
+  if (+plScore.textContent === 2 || +comScore.textContent === 2) {
+    whoWon();
+  }
+};
+
+const startGame = function () {
+  game();
+};
+
+const getPick = function () {};
+
+start.addEventListener("click", startGame);
+
+newGame.addEventListener("click", startGame);
